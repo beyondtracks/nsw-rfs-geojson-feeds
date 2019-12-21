5dc897a88142d46bdc46dc0369b8ef9405aa50e7
Andrew Harvey <andrew@beyondtracks.com>
2019-12-22 00:33:29 +1100

    fix e2e test

--- e2eTest.sh
+++ e2eTest.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-/bin/nsw-rfs-majorincidents-geojson --pretty-print nsw-rfs-majorincidents.geojson
+./bin/nsw-rfs-majorincidents-geojson --pretty-print nsw-rfs-majorincidents.geojson
 geojsonhint nsw-rfs-majorincidents.geojson
 
-/bin/nsw-rfs-majorincidents-geojson --pretty-print nsw-rfs-majorincidents-avoidgeometrycollections.geojson
+./bin/nsw-rfs-majorincidents-geojson --pretty-print nsw-rfs-majorincidents-avoidgeometrycollections.geojson
 geojsonhint nsw-rfs-majorincidents-avoidgeometrycollections.geojson
 
-/bin/nsw-rfs-majorincidents-geojson --pretty-print nsw-rfs-majorincidents-avoidslivers.geojson
+./bin/nsw-rfs-majorincidents-geojson --pretty-print nsw-rfs-majorincidents-avoidslivers.geojson
 geojsonhint nsw-rfs-majorincidents-avoidslivers.geojson
