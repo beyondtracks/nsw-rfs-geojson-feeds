e180528b14362ded430942c2831b03fba9515097
Andrew Harvey <andrew@beyondtracks.com>
2020-10-17 14:42:22 +1100

    test fixtures

--- e2eTest.sh
+++ e2eTest.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
-./bin/nsw-rfs-majorincidents-geojson --pretty-print nsw-rfs-majorincidents.geojson
-geojsonhint nsw-rfs-majorincidents.geojson
+mkdir -p output
 
-./bin/nsw-rfs-majorincidents-geojson --pretty-print nsw-rfs-majorincidents-avoidgeometrycollections.geojson
-geojsonhint nsw-rfs-majorincidents-avoidgeometrycollections.geojson
+echo "Test 1: download and process major incidents"
+./bin/nsw-rfs-majorincidents-geojson --pretty-print output/nsw-rfs-majorincidents.geojson
+geojsonhint output/nsw-rfs-majorincidents.geojson
 
-./bin/nsw-rfs-majorincidents-geojson --pretty-print nsw-rfs-majorincidents-avoidslivers.geojson
-geojsonhint nsw-rfs-majorincidents-avoidslivers.geojson
+echo "Test 2: download and process major incidents --avoid-geometrycollections"
+./bin/nsw-rfs-majorincidents-geojson --pretty-print --avoid-geometrycollections output/nsw-rfs-majorincidents-avoidgeometrycollections.geojson
+geojsonhint output/nsw-rfs-majorincidents-avoidgeometrycollections.geojson
+
+echo "Test 3: download and process major incidents --avoid-slivers"
+./bin/nsw-rfs-majorincidents-geojson --pretty-print --avoid-slivers output/nsw-rfs-majorincidents-avoidslivers.geojson
+geojsonhint output/nsw-rfs-majorincidents-avoidslivers.geojson
+
+echo "Test 4: download and process hazard reduction"
+./bin/nsw-rfs-hazard-reduction-geojson --pretty-print output/nsw-rfs-hazard-reduction.geojson
+geojsonhint output/nsw-rfs-hazard-reduction.geojson
+
+echo "Test 5: input fixture hazard reduction"
+./bin/nsw-rfs-hazard-reduction-geojson --pretty-print test/fixtures/input/hr.json - | diff - test/fixtures/output/hr.geojson
+
