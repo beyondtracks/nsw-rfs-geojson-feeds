bb61a87888359896d759fc49f554374f81881a4f
Andrew Harvey <andrew@beyondtracks.com>
2020-10-17 15:24:49 +1100

    add index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const NSWRFSMajorIncidentsGeoJSON = require('../src/majorincidents')
+const NSWRFSHazardReduction = require('../src/hazardreduction')
+
+module.exports = {
+  NSWRFSMajorIncidentsGeoJSON,
+  NSWRFSHazardReduction
+}
